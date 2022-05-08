f633a5284dfafc7c1bdd1e4032757b05564f2148
fullStackImperator <stevanv@gmx.de>
2022-05-08 02:12:41 +0200

    added views and templates

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,12 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
 
 
 class Post(models.Model):
@@ -21,8 +26,16 @@ class Post(models.Model):
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
 
+    objects = models.Manager() # The default manager.
+    published = PublishedManager() # Our custom manager.
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.publish.year, self.publish.month, self.publish.day, self.slug])
+
+
     class Meta:
         ordering = ('-publish',)
         
     def __str__(self):
         return self.title
+
