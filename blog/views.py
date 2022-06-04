9095352d5a10011c8671ba028b601d8edab65f7e
fullStackImperator <stevanv@gmx.de>
2022-06-04 19:20:39 +0200

    added similar tags recommendation fpor post + first own template tag

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,7 @@ from django.views.generic import ListView
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from taggit.models import Tag
+from django.db.models import Count
 
 
 class PostListView(ListView):
@@ -16,7 +17,6 @@ class PostListView(ListView):
 
 
     
-
 def post_list(request, tag_slug=None):
 #    posts = Post.published.all()
     object_list = Post.published.all()
@@ -63,8 +63,14 @@ def post_detail(request, year, month, day, post):
     else:
         comment_form = CommentForm()
 
+
+    # List of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True) 
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags','-publish')[:4]
+
     context = {'post': post, 'comments':comments, 
-                'new_comment': new_comment, 'comment_form': comment_form}
+                'new_comment': new_comment, 'comment_form': comment_form, 'similar_posts': similar_posts}
                 
     return render(request, 'blog/post/detail.html', context)
 
